a6607c15dc6a0dc3b8aa58a27f20e963cea1332d
Maxwell Antonucci <maxwellantonucci@Maxwell-Antonuccis-MacBook-Pro.local>
2015-10-30 08:30:41 -0400

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,159 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    minifyCSS = require('gulp-minify-css'),
+    livereload = require('gulp-livereload'),
+    autoprefixer = require('gulp-autoprefixer'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    jade = require('gulp-jade'),
+    include = require('gulp-include'),
+    plumber = require('gulp-plumber'),
+    notify = require('gulp-notify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uncss = require('gulp-uncss'),
+    express = require('express'),
+    del = require('del');
+
+var express = require('express')
+var app = express()
+app.use('/', express.static(__dirname + '/build'))
+app.listen(3000)
+console.log('Express site on 3000!')
+
+var config = {
+    projectPath: 'build/',
+    assetsPath: 'build/assets/',
+    componentPath: 'components/'
+}
+
+// Find errors!
+function errorLog(error) {
+  console.error.bind(error);
+  this.emit('end');
+}
+
+// Watch the homepage!
+gulp.task('homepage', function(){
+    gulp.src('index.html')
+    .pipe(livereload());
+});
+
+// Uglify, to compress JS files
+gulp.task('scripts', function(){
+    gulp.src('js/main.js')
+    .pipe(include())
+      .on('error', console.log)
+    .pipe(plumber())
+         .pipe(uglify())
+    .on("error", notify.onError("Error:" + errorLog))
+    .pipe(rename('main.min.js'))
+    .pipe(gulp.dest(config.assetsPath + 'js'))
+    .pipe(notify({
+        message: 'JS Uglified!',
+        onLast: true
+    }))
+    .pipe(livereload());
+});
+
+// Convert all the SASS to CSS
+var sassInput = 'sass/main.scss';
+var sassOptions = { 
+    outputStyle: 'expanded' 
+};
+var autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+
+gulp.task('sass', function () {
+  return gulp
+    .src(sassInput)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .on("error", notify.onError("Error:" + errorLog))
+    .pipe(sourcemaps.write())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest(config.assetsPath + 'css'))
+    .pipe(notify({
+        message: 'Sass Processed!',
+        onLast: true
+    }))
+    .pipe(livereload());
+});
+
+gulp.task('uncss', function () {
+  return gulp
+    .src(config.assetsPath + 'css/style.min.css')
+    .pipe(uncss({
+        html: ['build/**/**/*.html']
+    }))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(config.assetsPath + 'css'))
+    .pipe(notify({
+        message: 'CSS Trimmed!',
+        onLast: true
+    }))
+});
+
+// Compress all the image things!
+gulp.task('images', function () {
+    return gulp.src('jade/img/*')
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .on("error", notify.onError("Error:" + errorLog))
+        .pipe(gulp.dest(config.assetsPath + '/img'))
+        .pipe(notify({
+        message: 'Images Optimized!',
+        onLast: true
+    }))
+        .pipe(livereload());
+});
+
+// Get all the Jade things!
+gulp.task('jade', function() {
+    var my_locals = {};
+
+    gulp.src('jade/**/**/*.jade')
+        .pipe(jade({
+            locals: my_locals
+        }))
+        // .on("error", notify.onError("Error:" + errorLog))
+        .pipe(gulp.dest(config.projectPath))
+        .pipe(notify({
+            message: 'HTML Jaded!',
+            onLast: true
+        }))
+        .pipe(livereload());
+});
+
+gulp.task('prod-init', function () {
+  return gulp
+    .src(sassInput)
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .on("error", notify.onError("Error:" + errorLog))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest(config.assetsPath + 'css'));
+});
+
+gulp.task('clean', function () {
+    return del([
+        'build/extends'
+    ]);
+});
+
+
+
+// Task to watch the things!
+gulp.task('watch', function(){
+  livereload.listen();
+    gulp.watch('js/**/**/*.js', ['scripts']);
+    gulp.watch('sass/**/**/*.scss', ['sass']);
+    gulp.watch('jade/**/**/*.jade', ['jade']);
+    gulp.watch('jade/img/**/**/*', ['images']);
+    gulp.watch('index.html', ['homepage']);
+});
+
+gulp.task('default', ['scripts', 'sass', 'jade', 'images', 'watch']);
+gulp.task('prod', ['clean', 'prod-init', 'uncss']);
---
+++ js/main.js
@@ -0,0 +1,23 @@
+//=require ../components/jquery/dist/jquery.min.js
+
+$(document).ready(function () {
+
+// Components
+
+// Utilities
+//=require utils/variables.js
+//=require utils/functions.js
+
+// Base
+//=require base/base.js
+
+// Components
+//=require components/components.js
+
+// Pages
+//=require pages/page.js
+
+// Apps
+//=require apps/app.js
+
+}); //Doc ready is done!
